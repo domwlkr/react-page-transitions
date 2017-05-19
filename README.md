b58718c2265d2aaa1b180856d50b9abbeac74303
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2017-05-19 17:08:54 +0100

    Removed more redundant files and updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# react-page-transitions
+React Page Transitions
+==========
+
 Quick test project to demo route transitions in a react app
+
+## Getting started
+
+    git clone git@github.com:domwlkr/react-page-transitions.git
+    cd react-page-transitions
+    npm install
--- src/containers/App.js
+++ src/containers/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Header />
+                
                 <main className="main">
                     <Route render={({location, history, match}) => {
                         return (
